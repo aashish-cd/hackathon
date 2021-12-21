8e66dc144393388d4eb025d160a162096e9ebb43
aashish-cd <076bct002@ioepc.edu.np>
2021-12-22 04:58:15 +0545

    error

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from '../component/Navbar/Navbar'
-import Ambulance from '../component/Ambulance/Ambulance'
-import Appointment from '../component/Appointment/Appointment'
-import Carasoul from '../component/Carasoul/Carasoul'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Navbar from '../component/Navbar/Navbar';
+import Ambulance from '../component/Ambulance/Ambulance';
+import Appointment from '../component/Appointment/Appointment';
+import Carasoul from '../component/Carasoul/Carasoul';
 export default function Home() {
   return (
-      <>
+    <>
       {/* <Navbar /> */}
       {/* <Ambulance /> */}
-      < Appointment />
-      <Carasoul/>
-      </>
-  )
-};
+
+      <Appointment />
+      <Carasoul />
+    </>
+  );
+}
