71a9f3685c4c7ecd1f79cd3ccd4a101a8bf4f0e4
Undef125 <076bct038@ioepc.edu.np>
2021-12-21 19:11:30 +0545

    2nd

---
+++ component/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './style.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={style.Navbar}>
+            <li>Home</li>
+            <li>About Us</li>
+        </div>
+    )
+}
+
+export default Navbar
