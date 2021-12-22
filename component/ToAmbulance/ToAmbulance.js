24c3a0c9d957a9379bb87f9d8c62de8baf5836b9
Undef125 <076bct038@ioepc.edu.np>
2021-12-22 06:12:00 +0545

    to pull

--- component/ToAmbulance/ToAmbulance.js
+++ component/ToAmbulance/ToAmbulance.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import style from './tamb.module.css'
-import Link from 'next/link'
+import React from "react";
+import style from "./tamb.module.css";
+import Link from "next/link";
 
 const ToAmbulance = () => {
     return (
-        <div>
             <div className={style.wholeToAppointContainer}>
-            <div className={style.details}>
-                <h2>Find Ambulance</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quidem, odit repellat iste repellendus aliquid ullam nobis possimus quas quaerat, sint recusandae debitis et consectetur facilis unde veritatis neque deserunt!</p>
-                <Link href='/Ambulance/Ambulance'>
-                <button>Search For An Ambulance</button>
-                </Link>
+                <div className={style.details}>
+                    <h2>Find Ambulance</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Totam quidem, odit repellat iste repellendus aliqui
+                        ullam nobis possimus quas quaerat, sint recusandae
+                        debitis et consectetur facilis unde veritatis neque
+                        deserunt!
+                    </p>
+                    <Link href="/Ambulance/Ambulance">
+                        <button>Search For An Ambulance</button>
+                    </Link>
+                </div>
+                <div className={style.imageHolder}>
+                    <img src="./amb.png" alt="" />
+                </div>
             </div>
-            <div className={style.imageHolder}>
-                <img src="./amb.png" alt="" />
-            </div>
-        </div>
-        </div>
-    )
-}
+    );
+};
 
-export default ToAmbulance
+export default ToAmbulance;
