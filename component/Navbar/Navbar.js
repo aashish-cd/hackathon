075802f13d9c02cc84c70892f70b21665c7b0054
aashish-cd <076bct002@ioepc.edu.np>
2021-12-22 05:48:04 +0545

    some changes

--- component/Navbar/Navbar.js
+++ component/Navbar/Navbar.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import style from './style.module.css'
+import React from 'react';
+import style from './style.module.css';
+import Link from 'next/link';
 
 const Navbar = () => {
-    return (
-        <div className={style.Navbar}>
-            <li>Home</li>
-            <li>About Us</li>
-        </div>
-    )
-}
+  return (
+    <div className={style.Navbar}>
+      <li>
+        <Link href='/'>Home</Link>
+      </li>
+      <li>
+        <Link href='/Ambulance/Ambulance'>Ambulance</Link>
+      </li>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
